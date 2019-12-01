9b41759f6d700e837848676faecde1fcf68439ae
Holly Davis <hollydavis541@gmail.com>
2019-11-30 23:26:55 -0800

    added pie and bar charts

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 
 $(document).ready(() => {
 
-  var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-  var locationHourlySales = [];
-  var locationData = [
+  const storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+  const locationHourlySales = [];
+  const locationData = [
     ['Seattle', 23, 65, 6.3 , [], 0],
     ['Tokyo', 3, 24, 1.2, [], 0],
     ['Dubai', 11, 38, 3.7, [], 0],
@@ -12,7 +12,7 @@ $(document).ready(() => {
     ['Lima', 2, 16, 4.6, [], 0]
   ];
 
-  var Store = function(city, minHourlyCustomers, maxHourlyCustomers, avgCookiesPerCust, hourlySales, dailySales){
+  const Store = function(city, minHourlyCustomers, maxHourlyCustomers, avgCookiesPerCust, hourlySales, dailySales){
     this.city = city;
     this.minHourlyCustomers = minHourlyCustomers;
     this.maxHourlyCustomers = maxHourlyCustomers;
@@ -30,8 +30,8 @@ $(document).ready(() => {
   };
 
   Store.prototype.cookiesPerHour = function() {
-    for (var i = 0; i < storeHours.length; i++) {
-      var cookieSales = Math.round(this.customerRandomizer() * this.avgCookiesPerCust);
+    for (let i = 0; i < storeHours.length; i++) {
+      let cookieSales = Math.round(this.customerRandomizer() * this.avgCookiesPerCust);
       this.hourlySales.push(cookieSales);
     }
     locationHourlySales.push(this.hourlySales);
@@ -39,14 +39,13 @@ $(document).ready(() => {
 
   Store.prototype.cookiesPerDay = function() {
     this.dailySales = 0;
-    for (var i = 0; i < storeHours.length; i++) {
+    for (let i = 0; i < storeHours.length; i++) {
       this.dailySales += this.hourlySales[i];
     }
   };
 
   function renderTableHeader() {
     $('#cookiesales').append('<table>');
-    $('table').attr('id', 'table'); // remove once refactoring complete
     $('table').append('<thead>');
     $('thead').append('<tr>');
     $('tr').append('<th>');
@@ -62,10 +61,10 @@ $(document).ready(() => {
     $('tfoot tr').attr('id', 'totals');
     $('tfoot tr').append('<td>Hourly Total</td>');
 
-    var columnTotal = 0;
-    for(var i = 0; i < storeHours.length; i++){
-      var hourTotal = 0;
-      for(var j = 0; j < locationHourlySales.length; j++){
+    let columnTotal = 0;
+    for(let i = 0; i < storeHours.length; i++){
+      let hourTotal = 0;
+      for(let j = 0; j < locationHourlySales.length; j++){
         hourTotal += parseInt(locationHourlySales[j][i]);
       }
       columnTotal += hourTotal;
@@ -77,8 +76,8 @@ $(document).ready(() => {
   Store.prototype.renderSalesData = function(){
     $('tbody').append('<tr>');
     $('tbody tr').append(`<th>${this.city}</th>`);
-    for(var i = 0; i < storeHours.length; i++){
-      var sales = this.hourlySales[i];
+    for(let i = 0; i < storeHours.length; i++){
+      let sales = this.hourlySales[i];
       $('tbody tr').append(`<td>${sales}</td>`);
     }
     $('tbody tr').append(`<td>${this.dailySales}</td>`);
@@ -89,58 +88,54 @@ $(document).ready(() => {
 
   function handleSubmit(e){
     e.preventDefault();
-    var city = e.target.city.value;
-    var mincustomers = e.target.mincustomers.value;
-    var maxcustomers = e.target.maxcustomers.value;
-    var avgcookies = e.target.avgcookies.value;
-    var newStore = new Store(city, mincustomers, maxcustomers, avgcookies, [], 0);
+    let city = e.target.city.value;
+    let mincustomers = e.target.mincustomers.value;
+    let maxcustomers = e.target.maxcustomers.value;
+    let avgcookies = e.target.avgcookies.value;
+    let newStore = new Store(city, mincustomers, maxcustomers, avgcookies, [], 0);
     newStore.cookiesPerHour();
     newStore.cookiesPerDay();
     newStore.renderSalesData();
     removeFooter();
     renderTableFooter();
-    makeChart();
+    makePieChart();
+    makeBarChart();
   }
 
   function removeFooter() {
     $('tfoot').remove();
   }
 
-  // Chart Generator
-  var genLabels = function(images) {
-    var labelsArr = [];
-    for (var i=0; i < images.length; i++){
-      labelsArr.push(images[i].city);
+  ////////////////////////////////////////
+  //
+  // Pie Chart - Daily Sales
+  //
+  ////////////////////////////////////////
+  let getPieLabels = function(info) {
+    const labelsArr = [];
+    for (let i=0; i < info.length; i++){
+      labelsArr.push(info[i].city);
     }
     return labelsArr;
   };
 
-  var genData01 = function(images) {
-    var dataArr = [];
-    for (var i=0; i < images.length; i++){
-      dataArr.push(images[i].clicks);
+  let genPieData = function(info) {
+    const dataArr = [];
+    for (let i=0; i < info.length; i++){
+      dataArr.push(info[i].dailySales);
     }
     return dataArr;
   };
 
-  var genData02 = function(images) {
-    var dataArr = [];
-    for (var i=0; i < images.length; i++){
-      dataArr.push(images[i].timesShown);
-    }
-    return dataArr;
-  };
-
-  function makeChart(){
-
-    var ctx = document.getElementById('canvas-chart').getContext('2d');
-    var productChart = new Chart(ctx, {
-      type: 'bar',
+  function makePieChart(){
+    const ctx = document.getElementById('canvas-chart').getContext('2d');
+    const productChart = new Chart(ctx, {
+      type: 'pie',
       data: {
-        labels: genLabels(Store.data),
+        labels: getPieLabels(Store.data),
         datasets: [{
-          label: 'Votes',
-          data: genData01(Store.data),
+          label: 'Daily Sales',
+          data: genPieData(Store.data),
           backgroundColor: [
             'rgba(255, 99, 132, 0.7)',
             'rgba(54, 162, 235, 0.7)',
@@ -187,30 +182,65 @@ $(document).ready(() => {
           ],
           borderWidth: 1
         },
-        {
-          label: 'Appearances',
-          data: genData02(Store.data),
+        ],
+      },
+    });
+  }
+
+  ////////////////////////////////////////
+  //
+  // Bar Chart - Hourly Totals
+  //
+  ////////////////////////////////////////
+  let getBarLabels = function(info) {
+    const labelsArr = [];
+    for (let i=0; i < info.length; i++){
+      labelsArr.push(info[i]);
+    }
+    return labelsArr;
+  };
+
+  let genBarData = function() {
+    const dataArr = [];
+    $('tfoot td').map(function() {
+      dataArr.push($(this).text());
+    });
+    dataArr.shift();
+    dataArr.pop();
+    return dataArr;
+  };
+
+  function makeBarChart(){
+
+    const ctx = document.getElementById('canvas-chart02').getContext('2d');
+    const productChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: getBarLabels(storeHours),
+        datasets: [{
+          label: 'Hourly Sales',
+          data: genBarData(Store.data),
           backgroundColor: [
-            'rgba(255, 99, 132, 0.4)',
-            'rgba(54, 162, 235, 0.4)',
-            'rgba(255, 206, 86, 0.4)',
-            'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)',
-            'rgba(255, 99, 132, 0.4)',
-            'rgba(54, 162, 235, 0.4)',
-            'rgba(255, 206, 86, 0.4)',
-            'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)',
-            'rgba(255, 99, 132, 0.4)',
-            'rgba(54, 162, 235, 0.4)',
-            'rgba(255, 206, 86, 0.4)',
-            'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)',
-            'rgba(255, 99, 132, 0.4)',
-            'rgba(54, 162, 235, 0.4)',
-            'rgba(255, 206, 86, 0.4)',
-            'rgba(75, 192, 192, 0.4)',
-            'rgba(153, 102, 255, 0.4)'
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)',
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(153, 102, 255, 0.7)'
           ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
@@ -235,7 +265,7 @@ $(document).ready(() => {
             'rgba(153, 102, 255, 1)'
           ],
           borderWidth: 1
-        }
+        },
         ],
       },
       options: {
@@ -243,8 +273,6 @@ $(document).ready(() => {
           yAxes: [{
             ticks: {
               beginAtZero: true,
-              // Found stepSize info at https://www.chartjs.org/docs/latest/axes/cartesian/linear.html
-              stepSize: 1
             }
           }]
         }
@@ -252,18 +280,19 @@ $(document).ready(() => {
     });
   }
 
+  // IIFE - Makes all the things show up on the page :)
   (function renderAll(data){
     renderTableHeader();
     $('table').append('<tbody>');
-    for(var i = 0; i < data.length; i++){
+    for(let i = 0; i < data.length; i++){
       data[i] = new Store(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5]);
       data[i].cookiesPerHour();
       data[i].cookiesPerDay();
       data[i].renderSalesData();
     }
     renderTableFooter();
-    console.log(Store.data);
-    makeChart();
+    makePieChart();
+    makeBarChart();
   })(locationData);
 
 });
